6ae58eac09e1c19ca2157204a483d38892cfbd65
Audrunas Matonis <audrunas@devbridge.com>
2019-10-30 15:13:37 +0200

    Pirmasis demo kodas

---
+++ script.js
@@ -0,0 +1,13 @@
+// var allBtn = document.querySelectorAll('.my_btn');
+// var body = document.querySelector('body');
+//
+// allBtn.forEach(function (item, idx) {
+//     item.addEventListener('click', function (e) {
+//         body.setAttribute('style', 'background: #'+(Math.random()*0xFFFFFF<<0).toString(16));
+//         allBtn.setAttribute('class', 'disabled');
+//         item.setAttribute('style', 'background: #'+(Math.random()*0xFFFFFF<<0).toString(16));
+//         e.preventDefault();
+//     })
+// });
+
+'#'+(Math.random()*0xFFFFFF<<0).toString(16);
